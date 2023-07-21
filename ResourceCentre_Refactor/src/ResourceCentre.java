3a300402ba1de525542ba311f35bb19b6d26d73f
22026391 <22026391@LAPTOP-AKO8QBM6>
2023-07-21 11:48:20 +0800

    updated

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -3,18 +3,18 @@ import java.util.ArrayList;
 public class ResourceCentre {
 
 
-<<<<<<< HEAD
+
 	private static final int ITEM_TYPE_CHROMEBOOK = 2;
 	private static final int ITEM_TYPE_CAMCODER = 1;
 	private static final int OPTION_RETURN = 4;
 	private static final int OPTION_LOAN = 3;
 	private static final int OPTION_ADD = 2;
 	private static final int OPTION_VIEW = 1;
-=======
+
 	/**
 	 * 
 	 */
->>>>>>> stash
+
 	private static final int OPTION_QUIT = 5;
 
 	public static void main(String[] args) {
